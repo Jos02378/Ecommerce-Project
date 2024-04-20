f535b157bb87cae6710a5876b2ea14cab41470d8
Joseph Haryanto <57058570+Jos02378@users.noreply.github.com>
2024-04-20 19:05:39 +0700

    feat: add cross origin support

--- backend/spring-boot-ecommerce/src/main/java/com/joseph/ecommerce/dao/ProductCategoryRepository.java
+++ backend/spring-boot-ecommerce/src/main/java/com/joseph/ecommerce/dao/ProductCategoryRepository.java
@@ -3,8 +3,10 @@ package com.joseph.ecommerce.dao;
 import com.joseph.ecommerce.entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 // change the default endpoint from productCategories to product-category
+@CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(collectionResourceRel = "productCategory", path="product-category")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 }
